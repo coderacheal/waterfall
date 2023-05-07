5be36b8f7778da92212c2c5861b360fa40697c16
Racheal Appiah-kubi <coderacheal@gmail.com>
2023-05-07 14:31:29 +0000

    Complete login component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const Login = () => (
-  <div>
+  <div className='loginDiv'>
     <form action="">
-      <input type="text" placeholder='Enter your email'/>
-      <input type="text" placeholder='Enter your password'/>
-      <button>Log in</button>
+      <input type="text" placeholder='Enter your email' className='login_inputs'/><br />
+      <input type="text" placeholder='Enter your password' className='login_inputs'/><br />
+      <button className='submit-login-credentials'>Log in</button>
     </form>
   </div>
 );
